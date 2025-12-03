42b64c242a80884d7efbe329d3b051852da619d9
Rose Ziech <rziech@scls.info>
2025-12-03 16:46:32 -0600

    Update cert_deploy.php to require common.php

--- acquia_cloud_cert_deployment/cert_deploy.php
+++ acquia_cloud_cert_deployment/cert_deploy.php
@@ -4,6 +4,7 @@
 // This example requires `league/oauth2-client` package.
 // Run `composer require league/oauth2-client` before running.
 require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/common.php';
 
 use League\OAuth2\Client\Provider\GenericProvider;
 use GuzzleHttp\Client;
@@ -146,78 +147,4 @@ if ($response->getStatusCode() == 202 && $cmd['activate']) {
       }
     }
   }
-}
-
-/**
- * Helper function, which extracts saved secrets from secrets.settings.php file.
- */
-function extract_secrets($cmd) {
-  // Load Acquia Cloud secrets file
-  $secrets_file = sprintf('/mnt/files/%s.%s/secrets.settings.php', $_ENV['AH_SITE_GROUP'], $_ENV['AH_SITE_ENVIRONMENT']);
-
-  if (!file_exists($secrets_file)) {
-    $cmd->error(new Exception('The secrets file wasn\'t found. Please read https://docs.acquia.com/resource/secrets/ and create one.'));
-  }
-
-  require $secrets_file;
-
-  if (!isset($acquia_cloud_token, $acquia_cloud_secret)) {
-    $cmd->error(new Exception('The script needs variables $acquia_cloud_token and $acquia_cloud_secret defined in the secrets.settings.php file.'));
-  }
-
-  return [
-    'token' => $acquia_cloud_token,
-    'secret' => $acquia_cloud_secret,
-  ];
-}
-
-/**
- * Helper function, returns array of deployed certificates, keyed by
- * certificate ID.
- *
- * @param $environment_id
- * @param $client
- * @param $base_url
- * @param $cmd
- *
- * @return array
- */
-function get_deployed_certificates($environment_id, $client, $base_url, $cmd, $provider, $accessToken) {
-  // Request.
-  try {
-    $api_method = "environments/{$environment_id}/ssl/certificates";
-    $response = $client->send($provider->getAuthenticatedRequest('GET', $base_url . $api_method, $accessToken, []));
-  } catch (ClientException $e) {
-        $cmd->error($e->getMessage());
-  }
-
-  if ($deployed_certificates = json_decode($response->getBody(), TRUE)) {
-    $certificates_map = [];
-    foreach ($deployed_certificates['_embedded']['items'] as $item) {
-      $certificates_map[$item['id']] = $item['label'];
-    }
-    return $certificates_map;
-  }
-}
-
-/**
- * Prints response from Acquia Cloud API.
- *
- * @param $response_body
- * @param $cmd
- */
-function print_response_message($response_body, $cmd) {
-  // If it's a Response object, extract the body content
-  if ($response_body instanceof \Psr\Http\Message\ResponseInterface) {
-    $response_body = $response_body->getBody()->getContents();
-  }
-
-  $response_body = json_decode($response_body, TRUE);
-
-  if (array_key_exists('error', $response_body)) {
-    $message = is_array($response_body['message']) ? json_encode($response_body['message']) : $response_body['message'];
-    $cmd->error(new Exception($message));
-  }
-
-  print $response_body['message'] . PHP_EOL;
 }
